17b7aaaec3e360782185ad9746134b488e4b4fea
Robson6x6 <robson6x6@gmail.com>
2025-06-20 17:16:24 -0300

    adicionando modelos de comentarios

--- estilos-de-códigos.lua
+++ estilos-de-códigos.lua
@@ -1,9 +1,24 @@
 local mais_ou_menos_assim = 1 -- snake_case
+
+
+----------------------------------
+--[[
+    varias linhas
+    pra construir
+    os comentários
+    em blocos
+]]
+
+-- especial para documentação
+
+--- Contém o nome do monstro no exemplo
+local MONSTER_NAME <const> = "Creeper"
+
 local isAngry = 1 --camelCase
 local MonterName = 1 --PascalCase
 local MONSTER_NAME -- SCREAMING_SNAKE_CASE
 
-local MONSTER_NAME <const> = "Creeper"
+
 
 
 print(MONSTER_NAME)
