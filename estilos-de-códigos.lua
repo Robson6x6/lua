a82c6c2b182831d7501879dd54cfb1e4f3897254
Robson6x6 <robson6x6@gmail.com>
2025-06-20 16:58:20 -0300

    estudo estilo de código

---
+++ estilos-de-códigos.lua
@@ -0,0 +1,9 @@
+local mais_ou_menos_assim = 1 -- snake_case
+local isAngry = 1 --camelCase
+local MonterName = 1 --PascalCase
+local MONSTER_NAME -- SCREAMING_SNAKE_CASE
+
+local MONSTER_NAME <const> = "Creeper"
+
+
+print(MONSTER_NAME)
