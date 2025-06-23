11b89e3f34d85c88995def442cfa1a7db2d04ac7
Robson6x6 <robson6x6@gmail.com>
2025-06-23 03:04:53 -0300

    enviado com save correto

--- operador-de-diferente.lua
+++ operador-de-diferente.lua
@@ -1,4 +1,4 @@
---AND e OR
+--operador de diferente
 
 local NAME_MONSTER <const> = "Creeper"
 
@@ -9,6 +9,10 @@ local healthPercentage = health / maxHealth
 local isHealthCritical = healthPercentage <= 0.25
 
 health = health -1
+
+-- aqui a variavel foi recriada, pois na linha 7 ela pega o valor do health da linha 6
+-- na linha 11 o health foi redefinido então a variavel também tem que ser
+
 isHealthFull = health == maxHealth
 
 --local hasTakenDamage = health ~= maxHealth
